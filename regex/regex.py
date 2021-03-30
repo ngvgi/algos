cdb4ec459c9840007cebd46b279f316b0e9f9a08
ngvgi <anthonyjude554@gmail.com>
2021-03-30 16:22:24 +0300

    Progress in learning.

--- regex/regex.py
+++ regex/regex.py
@@ -18,13 +18,49 @@ Mr Smith
 Ms Davis
 Mrs. Robinson
 Mr. T
+Mr.Smidcht
 
 '''
 
 sentence = 'Start a sentence and then bring it to an end'
-pattern = re.compile(r'\d')
 
+# match any digit
+# pattern = re.compile(r'\d')
+
+# match any 'word' character(a-z,A-z,0-9)
+# pattern = re.compile(r'\w')
+
+# match any space (tab, space, newline) 
+# pattern = re.compile(r'\s')
+
+# match any space (tab, space, newline) 
+# pattern = re.compile(r'\s')
+
+# match any number separated by a dash or dot
+# pattern = re.compile(r'\d\d\d[-.]\d\d\d[-.]\d\d\d\d')
+# pattern = re.compile(r'\d{3}[-.]\d{3}[-.]\d{4}')
+
+# match 800 or 900 at the beginning
+# pattern = re.compile(r'[89]00[-.]\d\d\d[-.]\d\d\d\d')
+
+# match all the misters in text_to_search
+# pattern = re.compile(r'Mr\.?\s?[A-Z]\w*')
+
+# match all Mr. Mrs. Ms. regardless of whether they have the period
+# pattern = re.compile(r'M(r|s|rs)\.?\s?[A-Z]\w*')
+
+
+
+
+
+pattern = re.compile(r'M(r|s|rs)\.?\s?[A-Z]\w*')
 matches = pattern.finditer(text_to_search)
+
+# with open('data.txt', 'r') as f:
+#     contents = f.read()
+#     matches = pattern.finditer(contents)
+#     for match in matches:
+#         print(match)
 
 for match in matches:
     print (match)
